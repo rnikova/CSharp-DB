c104075fe6e1c1e3e2010ff8a1bf2341ca95cead
Rosmari Nikova <rosmari.nikova@yahoo.com>
2019-10-26 23:46:31 +0300

    Added DbSet and DbContext classes

--- Entity Framework Core/ORM Fundamentals/MiniORM/ChangeTracker.cs
+++ Entity Framework Core/ORM Fundamentals/MiniORM/ChangeTracker.cs
@@ -1,7 +1,10 @@
 ﻿namespace MiniORM
 {
-    using System.Collections.Generic;
+    using System;
     using System.Linq;
+    using System.Reflection;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
 
     internal class ChangeTracker<T>
         where T : class, new()
@@ -20,11 +23,86 @@
             this.allEntities = CloneEntities(entities);
         }
 
+        public IReadOnlyCollection<T> AllEntities => this.allEntities.AsReadOnly();
+
+        public IReadOnlyCollection<T> Added => this.added.AsReadOnly();
+
+        public IReadOnlyCollection<T> Removed => this.removed.AsReadOnly();
+
+        public void Add(T item) => this.added.Add(item);
+
+        public void Remove(T item) => this.removed.Add(item);
+
         private static List<T> CloneEntities(IEnumerable<T> entities)
         {
             List<T> clonedEntities = new List<T>();
 
-            var propertiesToClone = typeof(T).GetProperties().Where(x => DBContext)
+            var propertiesToClone = typeof(T)
+                .GetProperties()
+                .Where(x => DBContext.AllowedSqlTypes.Contains(x.PropertyType))
+                .ToArray();
+
+            foreach (var entity in entities)
+            {
+                var clonedEntity = Activator.CreateInstance<T>();
+
+                foreach (var property in propertiesToClone)
+                {
+                    var value = property.GetValue(entity);
+                    property.SetValue(clonedEntity, value);
+                }
+
+                clonedEntities.Add(clonedEntity);
+            }
+
+            return clonedEntities;
+        }
+
+        private static bool IsModified(T proxyEntity, T realEntity)
+        {
+            var monitoredProperties = typeof(T)
+                .GetProperties()
+                .Where(x => DbContext.AllowedSqlTypes.Contains(x.PropertyType))
+                .ToArray();
+
+            var modifiedProperties = monitoredProperties
+                .Where(x => !Equals(x.GetValue(realEntity), x.GetValue(proxyEntity)))
+                .ToArray();
+
+            return modifiedProperties.Any();
+        }
+
+        private static IEnumerable<object> GetPrimaryKeyValues(IEnumerable<PropertyInfo> primaryKeys, T entity)
+        {
+            return primaryKeys.Select(x => x.GetValue(entity));
+        }
+
+        public IEnumerable<T> GetModifiedEntities(DbSet<T> dbSet)
+        {
+            List<T> modifiedEntities = new List<T>();
+
+            var primaryKeys = typeof(T)
+                .GetProperties()
+                .Where(x => x.HasAttribute<KeyAttribute>())
+                .ToArray();
+
+            foreach (var proxyEntity in this.AllEntities)
+            {
+                var primaryKeyValues = GetPrimaryKeyValues(primaryKeys, proxyEntity).ToArray();
+
+                var entity = dbSet
+                    .Entities
+                    .Single(x => GetPrimaryKeyValues(primaryKeys, x).SequenceEqual(primaryKeyValues));
+
+                var isModified = IsModified(proxyEntity, entity);
+
+                if (isModified)
+                {
+                    modifiedEntities.Add(entity);
+                }
+            }
+
+            return modifiedEntities;
         }
     }
 }
--- Entity Framework Core/ORM Fundamentals/MiniORM/DbContext.cs
+++ Entity Framework Core/ORM Fundamentals/MiniORM/DbContext.cs
@@ -1,4 +1,32 @@
 ﻿namespace MiniORM
 {
-	// TODO: Create your DbContext class here.
+    using System;
+    using System.Reflection;
+    using System.Collections.Generic;
+
+    public abstract class DbContext
+    {
+        private readonly DatabaseConnection connection;
+
+        private readonly Dictionary<Type, PropertyInfo> DbSetProperties;
+
+        protected DbContext(string connectionString)
+        {
+            this.connection = new DatabaseConnection(connectionString);
+
+
+        }
+
+        internal static readonly Type[] AllowedSqlTypes =
+        {
+            typeof(string),
+            typeof(int),
+            typeof(uint),
+            typeof(long),
+            typeof(ulong),
+            typeof(decimal),
+            typeof(bool),
+            typeof(DateTime),
+        };
+    }
 }
