9f4224b3599be40fd6da7d3f880ff017c9256f68
Rosmari Nikova <rosmari.nikova@yahoo.com>
2019-10-26 21:44:05 +0300

    Implement ChangeTracker class from MiniORM homework

---
+++ Entity Framework Core/ORM Fundamentals/MiniORM/ChangeTracker.cs
@@ -0,0 +1,30 @@
+﻿namespace MiniORM
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class ChangeTracker<T>
+        where T : class, new()
+    {
+        private readonly List<T> allEntities;
+
+        private readonly List<T> added;
+
+        private readonly List<T> removed;
+
+        public ChangeTracker(IEnumerable<T> entities)
+        {
+            this.added = new List<T>();
+            this.removed = new List<T>();
+
+            this.allEntities = CloneEntities(entities);
+        }
+
+        private static List<T> CloneEntities(IEnumerable<T> entities)
+        {
+            List<T> clonedEntities = new List<T>();
+
+            var propertiesToClone = typeof(T).GetProperties().Where(x => DBContext)
+        }
+    }
+}
