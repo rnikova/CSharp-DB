76f71cd7aafe887aa313b6cb94fbc47aaff88ae3
Rosmari Nikova <rosmari.nikova@yahoo.com>
2019-11-24 00:09:59 +0200

    Started Exam 07.04.2019

---
+++ Entity Framework Core/Exam 07.04.2019/Cinema/DataProcessor/ImportDto/CustomerTicketsDto.cs
@@ -0,0 +1,28 @@
+﻿namespace Cinema.DataProcessor.ImportDto
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Xml.Serialization;
+
+    [XmlType("Customer")]
+    public class CustomerTicketsDto
+    {
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Range(12, 110)]
+        public int Age { get; set; }
+
+        [Required]
+        [Range(typeof(decimal), "0.01", "79228162514264337593543950335")]
+        public decimal Balance { get; set; }
+
+        [XmlArray("Tickets")]
+        public TicketDto[] Tickets { get; set; }
+    }
+}
---
+++ Entity Framework Core/Exam 07.04.2019/Cinema/DataProcessor/ImportDto/ProjectionDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Cinema.DataProcessor.ImportDto
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Xml.Serialization;
+
+    [XmlType("Projection")]
+    public class ProjectionDto
+    {
+        [Required]
+        public int MovieId { get; set; }
+
+        [Required]
+        public int HallId { get; set; }
+
+        [Required]
+        public string DateTime { get; set; }
+    }
+}
