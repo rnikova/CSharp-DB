fb1c114991a18a7fea6b4e700d5e483fbb7a7713
Rosmari Nikova <rosmari.nikova@yahoo.com>
2019-12-03 23:36:26 +0200

    Started Exam Preparation 3

---
+++ Entity Framework Core/Exam Preparation 3/VaporStore/Data/Models/Purchase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using VaporStore.Data.Enums;
+
+namespace VaporStore.Data.Models
+{
+    public class Purchase
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public PurchaseType Type { get; set; }
+
+        [Required]
+        [RegularExpression("^[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}$")]
+        public string ProductKey { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public int CardId { get; set; }
+        public Card Card { get; set; }
+
+        [Required]
+        public int GameId { get; set; }
+        public Game Game { get; set; }
+    }
+}
